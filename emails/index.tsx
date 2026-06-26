965bd9188c1adbe7892fc7832bbc59fb6b30f09b
Gonzalo <gonfarias6@gmail.com>
2026-06-26 17:22:24 -0300

    feat: recuperacion de contrasena

--- emails/index.tsx
+++ emails/index.tsx
@@ -10,3 +10,4 @@ export { AddonDeactivatedEmail } from './addon-deactivated'
 export { TrialExpiringSoonEmail } from './trial-expiring-soon'
 export { TrialExpiredEmail } from './trial-expired'
 
+export { PasswordResetEmail } from './password-reset'
